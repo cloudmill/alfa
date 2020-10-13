5b367cc8659ab618d76257dbd96e0a7b9b8f3de3
Alex <your_email@whatever.com>
2020-10-13 15:01:07 +0300

    update/accept set cookie

--- src/assets/scripts/backend.js
+++ src/assets/scripts/backend.js
@@ -1,1 +1,12 @@
-// backend
+$( document ).ready(function() {
+    $( ".ajax_accept_set_cookie" ).click(function() {
+        $.ajax({
+            type: "GET",
+            url: window.location.href,
+            data: 'accept_set_cookie=Y',
+            success: function(){
+                
+            }
+          });
+    });
+});
